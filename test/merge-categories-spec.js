7744d1e7e25573d7652e7764c2b9b2821b557176
Rob Whitacre <robert.w.whitacre@gmail.com>
2020-12-11 15:22:18 -0600

    first phase

--- test/merge-categories-spec.js
+++ test/merge-categories-spec.js
@@ -13,13 +13,48 @@ describe("mergeCategories()", () => {
 
     it("should return no <li>s for no categories", () => {
     });
+      //Arrange
+      const arr = []
+      //Act
+      const actual = mergeCategories(template, arr, 'li' )
+      //Assert
+      expect(actual).to.include('<div>')
+      expect(actual).to.include('</div>')
+      expect(actual).to.include('<ul>')
+      expect(actual).to.include('</ul>')
+      expect(actual).to.not.include('<li>')
+      expect(actual).to.not.include('</li>')
+      expect(actual).to.not.include('<!--Content here -->')
 
     it("should return a single <li> for one category", () => {
-      expect.fail('please write this test');
+      // Arrange
+      const arr = ['hello']
+      // Act
+      const actual = mergeCategories(template, arr, 'li')
+      // Assert
+      expect(actual).to.include('<div>')
+      expect(actual).to.include('</div>')
+      expect(actual).to.include('<ul>')
+      expect(actual).to.include('</ul>')
+      expect(actual).to.include('<li>hello</li>')
+      expect(actual).to.not.include('<!--Content here -->')
     });
 
     it("should return an <li> for each category", () => {
-      expect.fail('please write this test');
+      // Arrange
+      const arr = ['dog', 'cat', 'elephant']
+      // Act
+      const actual = mergeCategories(template, arr, 'li')
+      // Assert
+      expect(actual).to.include('<div>')
+      expect(actual).to.include('</div>')
+      expect(actual).to.include('<ul>')
+      expect(actual).to.include('</ul>')
+      expect(actual).to.include('<li>dog</li>')
+      expect(actual).to.include('<li>cat</li>')
+      expect(actual).to.include('<li>elephant</li>')
+      expect(actual).to.not.include('<!--Content here -->')
+
     });
   });
 
